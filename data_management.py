1a8f8a1def03c1d1e39d4d1fd05590d6da4fb5e1
Wendy051998 <wendyvelasquez650@gmail.com>
2025-03-06 10:24:12 -0800

    create a function to add a transaction

--- data_management.py
+++ data_management.py
@@ -28,6 +28,33 @@ def filter_transactions_by_date(sampledata):
     else:
         print(filtered.to_string(index=False))
 
+
+# Add a Transaction
+
+def add_transaction(sampledata):
+    # Load the file
+    df = pd.read_csv("sampledata.csv")
+
+    # Ask the user for the data they want to add
+    date = input("Enter the transaction date (YYYY-MM-DD): ")
+    category = input("Enter the category: ")
+    description = input("Enter the description: ")
+    amount = input("Enter the amount: ")
+    trans_type = input("Enter te type (Expense/Income): ")
+
+    # Create new DataFrame with the new transaction
+    new_transaction = pd.DataFrame({
+        "Date": [date],
+        "Category": [category],
+        "Description": [description],
+        "Amount": [amount],
+        "Type": [trans_type]
+    })
+
+    # Add the new transaction to the existing DataFrame
+    df = pd.concat([df, new_transaction], ignore_index = True)
+
 # To ensure that the correct file is uploaded and the processing works without errors
 sampledata = "sampledata.csv"
 filter_transactions_by_date(sampledata)
+add_transaction(sampledata)
